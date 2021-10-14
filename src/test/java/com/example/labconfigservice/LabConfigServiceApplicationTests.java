00d26e8ecf74a729951b1c8e20f6d00558d469f1
Jegor Chomczuk <jegor.chomczuk@gmail.com>
2021-10-14 14:11:41 +0200

    Initial commit

---
+++ src/test/java/com/example/labconfigservice/LabConfigServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.labconfigservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabConfigServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
